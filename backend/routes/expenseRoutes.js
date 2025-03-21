bc19f29d3489c8dcd15c4965d35bb8e3014a2ec8
UdayakumarMalaisamy <udayakumarmalaisamy@gmail.com>
2025-03-21 09:29:11 +0530

    Your commit message

--- backend/routes/expenseRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const expenseController = require('../controllers/expenseController');
-
-const router = express.Router();
-
-router.get('/', expenseController.getAllExpenses);
-router.post('/', expenseController.addExpense);
-
-module.exports = router;
